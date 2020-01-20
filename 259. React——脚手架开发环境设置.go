3f00ac77e079c6b7fb0cc707ac8c39d8566f6c5c
zhangjingwen <zhangjingwen2008@163.com>
2020-01-20 16:07:44 +0800

    React

---
+++ 259. React——脚手架开发环境设置.go
@@ -0,0 +1,24 @@
+package main
+
+/*
+	安装脚手架工具
+		1.设置npm淘宝镜像
+			- npm config set registry https://registry.npm.taobao.org
+		2.安装
+			- npm install -g create-react-app
+
+	生成应用项目目录
+		3.生成app
+			- create-react-app my-app
+		4.启动
+			- cd my-app
+			- npm start
+		5.生成上线文件
+			- npm run build
+
+	安装axios模块
+		1.终端的项目目录，执行命令
+			- npm install axios
+		2.在模块中引入
+			- import axios from 'axios';
+*/
